0b2e4a8e3b3c0313874912ec0a7c9f84c8b11898
NecroBread <mariovyordanov@gmail.com>
2022-06-18 13:54:31 +0300

    Add Post validators

--- src/models/Post.js
+++ src/models/Post.js
@@ -4,26 +4,42 @@ const postSchema = new Schema({
 	title: {
 		type: String,
 		required: true,
+		minlength: 6,
 	},
 	keyword: {
 		type: String,
 		required: true,
+		minlength: 6,
 	},
 	location: {
 		type: String,
 		required: true,
+		maxlength: 15,
 	},
 	creation_date: {
 		type: String,
 		required: true,
+		validate: {
+			validator: function (v) {
+				return v.length === 10;
+			},
+			message: 'Date should be 10 characters long'
+		},
 	},
 	image: {
 		type: String,
 		required: true,
+		validate: {
+			validator: function (v) {
+				return v.startsWith('http://') || v.startsWith('https://');
+			},
+			message: 'Image URL should be a valid link'
+		},
 	},
 	description: {
 		type: String,
 		required: true,
+		minlength: 10,
 	},
 	author: {
 		type: ObjectId,
