994a03e3e4a00bf12f4671938b8c0d455c647ccb
NecroBread <mariovyordanov@gmail.com>
2022-06-18 12:12:23 +0300

    Add visibility guard to details page buttons

--- src/controllers/postsController.js
+++ src/controllers/postsController.js
@@ -57,9 +57,11 @@ router.post('/edit/:id', isUser(), async (req, res) => {
 router.get('/delete/:id', async (req, res) => {
 	try {
 		const post = await getPostById(req.params.id);
+
 		if (req.session.user._id != post.author._id) {
 			throw new Error('Only owners can delete posts')
 		}
+
 		await deletePostById(req.params.id);
 		res.redirect('/posts');
 	} catch (err) {
@@ -71,6 +73,15 @@ router.get('/delete/:id', async (req, res) => {
 router.get('/details/:id', async (req, res) => {
 	try {
 		const post = await getPostById(req.params.id);
+
+		if (req.session?.user?._id == post.author._id) {
+			res.locals.isOwner = true;
+		}
+
+		if (post.votes.includes(res.session?.user?._id)) {
+			res.locals.hasVoted = true;
+		}
+
 		res.render('details', { title: post.title, post })
 	} catch (err) {
 		const errors = mapErrors(err);
